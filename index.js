e74a5e234eb49737bac8dd1f050cbd3c5c4c070a
gmneamul1 <gmneamul1@gmail.com>
2022-10-27 09:21:35 +0600

    category added

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-const categories = require('./data/category.json')
+app.use(cors());
+const categories = require("./data/category.json");
 
-const topics = require('./data/topics')
+const topics = require("./data/topics");
 
-app.get('/', (req, res) => {
-    res.send('new API Running');
+app.get("/", (req, res) => {
+  res.send("new API Running");
 });
 
-app.get('/course-categories', (req, res) => {
-    res.send(categories);
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
 });
 
-app.get('/category/:id', (req, res)=>{
-    const id  = req.params.id;
-    const category_topics = topics.filter(t =>t.category_id ==id );
-    res.send(category_topics)
-} )
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_topics = topics.filter((t) => t.category_id == id);
+  res.send(category_topics);
+});
 
-app.get('/topics', (req, res) => {
-    res.send(topics)
-})
+app.get("/topics", (req, res) => {
+  res.send(topics);
+});
 
-app.get('/topics/:id', (req, res ) =>{
-    const id = req.params.id
-    const selectTopics = topics.find(t => t._id ==id)
-    res.send(selectTopics)
-})
+app.get("/topics/:id", (req, res) => {
+  const id = req.params.id;
+  const selectTopics = topics.find((t) => t._id == id);
+  res.send(selectTopics);
+});
 
-
-app.listen(port, () =>{
-
-    console.log('server running ', port)
-})
+app.listen(port, () => {
+  console.log("server running ", port);
+});
